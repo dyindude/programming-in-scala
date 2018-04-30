c5ce6d6a90ebb88f123ef9dda532fdbda894721a
dyindude <dyindude@github.dyindude.net>
2018-04-30 13:32:24 -0500

    converted class parameters into class fields, add() method as on p103

--- Rational.scala
+++ Rational.scala
@@ -1,4 +1,11 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  override def toString = n + "/" + d
+  val numer: Int = n
+  val denom: Int = d
+  override def toString = numer + "/" + denom
+  def add(that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
 }
