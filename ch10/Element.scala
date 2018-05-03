2797930ba974b79012ef2fcd7627f42521bfafae
dyindude <dyindude@github.dyindude.net>
2018-05-03 15:07:31 -0500

    hding implementation with private classes ch10 p207

--- ch10/Element.scala
+++ ch10/Element.scala
@@ -15,12 +15,29 @@ abstract class Element {
   override def toString = contents mkString "\n"
 }
 
-class ArrayElement(
-  val contents: Array[String]
-) extends Element
+object Element {
+  private class ArrayElement(
+    val contents: Array[String]
+  ) extends Element
 
-class LineElement(s: String) extends Element {
-  val contents = Array(s)
-  override def width = s.length
-  override def height = 1
+  private class LineElement(s: String) extends Element {
+    val contents = Array(s)
+    override def width = s.length
+    override def height = 1
+  }
+
+  private class UniformElement(
+    ch: Char,
+    override val width: Int,
+    override val height: Int
+  ) extends Element {
+    private val line = ch.toString * width
+    def contents = Array.fill(height)(line)
+  }
+  def elem(contents: Array[String]): Element =
+    new ArrayElement(contents)
+  def elem(chr: Char, width: Int, height: Int): Element =
+    new UniformElement(chr, width, height)
+  def elem(line: String): Element =
+    new LineElement(line)
 }
