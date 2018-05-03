217e01fdd49e50d2e222d3dd6512ecbd4feffad6
dyindude <dyindude@github.dyindude.net>
2018-05-03 14:58:26 -0500

    first draft of above() method ch10 p201

--- ch10/Element.scala
+++ ch10/Element.scala
@@ -2,6 +2,9 @@ abstract class Element {
   def contents: Array[String]
   def height: Int = contents.length
   def width: Int = if (height == 0) 0 else contents(0).length
+
+  def above(that: Element): Element =
+    new ArrayElement(this.contents ++ that.contents)
 }
 
 class ArrayElement(
