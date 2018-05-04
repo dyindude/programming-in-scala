46d73014f42e69c82f4b647b289b5951e72577f5
dyindude <dyindude@github.dyindude.net>
2018-05-04 05:48:21 -0500

    completed chapter 10 spiral example, buggy but compiles

--- ch10/Element.scala
+++ ch10/Element.scala
@@ -1,20 +1,3 @@
-abstract class Element {
-  def contents: Array[String]
-  def height: Int = contents.length
-  def width: Int = if (height == 0) 0 else contents(0).length
-
-  def above(that: Element): Element =
-    new ArrayElement(this.contents ++ that.contents)
-  def beside(that: Element): Element = {
-    new ArrayElement(
-      for (
-           (line1, line2) <- this.contents zip that.contents
-         ) yield line1 + line2
-    )
-  }
-  override def toString = contents mkString "\n"
-}
-
 object Element {
   private class ArrayElement(
     val contents: Array[String]
@@ -41,3 +24,22 @@ object Element {
   def elem(line: String): Element =
     new LineElement(line)
 }
+import Element.elem
+
+abstract class Element {
+  def contents: Array[String]
+  def height: Int = contents.length
+  def width: Int = if (height == 0) 0 else contents(0).length
+
+  def above(that: Element): Element =
+    elem(this.contents ++ that.contents)
+  def beside(that: Element): Element = {
+    elem(
+      for (
+           (line1, line2) <- this.contents zip that.contents
+         ) yield line1 + line2
+    )
+  }
+  override def toString = contents mkString "\n"
+}
+
