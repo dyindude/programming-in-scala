2e3b5b6de049a6c8148755ecc6ae2134924a77cc
dyindude <dyindude@github.dyindude.net>
2018-05-04 09:40:31 -0500

    Start of IntQueue example ch12 p233

---
+++ ch12/IntQueue.scala
@@ -0,0 +1,12 @@
+abstract class IntQueue {
+  def get(): Int
+  def put(x: Int)
+}
+
+import scala.collection.mutable.ArrayBuffer
+
+class BasicIntQueue extends IntQueue {
+  private val buf = new ArrayBuffer[Int]
+  def get() = buf.remove(0)
+  def put(x: Int) = { buf += x }
+}
