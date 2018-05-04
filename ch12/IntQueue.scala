6390333f357621c3048f80f149b3e6f3a824bd6a
dyindude <dyindude@github.dyindude.net>
2018-05-04 09:46:46 -0500

    ch12 p235, example of supplying with Trait to new instead of defining a new class; demonstrate equality with assert()

--- ch12/IntQueue.scala
+++ ch12/IntQueue.scala
@@ -15,4 +15,10 @@ trait Doubling extends IntQueue {
   abstract override def put(x: Int) = { super.put(2 * x) }
 }
 
+//these are equivalent if we aren't making changes to the class
 class MyQueue extends BasicIntQueue with Doubling
+val q1 = new BasicIntQueue with Doubling
+val q2 = new MyQueue
+q1.put(10)
+q2.put(10)
+assert(q1.get == q2.get)
