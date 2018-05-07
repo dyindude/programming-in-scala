da5b162c7312a75271438b3b8b6076af0dba7ae6
dyindude <dyindude@github.dyindude.net>
2018-05-07 15:43:14 -0500

    added diff variants of tests, ch14 p265

--- ch14/src/test/scala/Element.scala
+++ ch14/src/test/scala/Element.scala
@@ -2,8 +2,20 @@ import org.scalatest.FunSuite
 import Element.elem
 
 class ElementSuite extends FunSuite {
-  test("elem result should have passed width") {
+  test("elem result should have passed width (using assert())") {
     val ele = elem('x', 2, 3)
     assert(ele.width == 2)
   }
+  test("elem result should have passed width (using assertResult())") {
+    val ele = elem('x', 2, 3)
+    assertResult(2) {
+      ele.width
+    }
+  }
+  //not implemented (yet)
+//  test("elem result should throw IllegalArgumentException for negative width") {
+//    assertThrows[IllegalArgumentException] {
+//      elem('x', -2, 3)
+//    }
+//  }
 }
