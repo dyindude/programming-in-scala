68d063564d7674048386b89e94d8ab2abdb2bd61
dyindude <dyindude@github.dyindude.net>
2018-05-09 09:54:58 -0500

    updated insertion sort algorithm using pattern matching ch16 p311

--- ch16/insertion-sort.scala
+++ ch16/insertion-sort.scala
@@ -1,12 +1,15 @@
 object InsertionSort {
   def main {
-    def isort(xs: List[Int]): List[Int] =
-      if (xs.isEmpty) Nil
-      else insert(xs.head, isort(xs.tail))
-    def insert(x: Int, xs: List[Int]): List[Int] =
-      if (xs.isEmpty || x <= xs.head) x :: xs
-      else xs.head :: insert(x, xs.tail)
-
+    def isort(xs: List[Int]): List[Int] = xs match {
+      case List() => List()
+      case x :: xs1 => insert(x, isort(xs1))
+    }
+    def insert(x: Int, xs: List[Int]): List[Int] = xs match {
+      case List() => List(x)
+      case y :: ys => if (x <= y) x :: xs
+                      else y :: insert(x, ys)
+    }
+    
     val fruit: List[String] = List("apples", "oranges", "pears")
     val a :: b :: rest = fruit
   }
