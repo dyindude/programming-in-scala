9f324f3366c0f5e94ee32f5639c96a5ceddec6d6
dyindude <dyindude@github.dyindude.net>
2018-05-09 11:17:12 -0500

    compare map() to flatMap() ch16 p325

--- ch16/higher-order-methods.scala
+++ ch16/higher-order-methods.scala
@@ -4,5 +4,13 @@ object HigherOrder {
     val words = List("the", "quick", "brown", "fox")
     assert((words map(_.length)) == List(3, 5, 5, 3))
     assert((words map(_.toList.reverse.mkString)) == List("eht", "kciuq", "nworb", "xof"))
+    assert((words map(_.toList)) == List[List[Char]](List('t', 'h', 'e'),
+                                                     List('q', 'u', 'i', 'c', 'k'),
+                                                     List('b', 'r', 'o', 'w', 'n'),
+                                                     List('f', 'o', 'x')))
+    assert((words flatMap(_.toList)) == List[Char]('t', 'h', 'e',
+                                                   'q', 'u', 'i', 'c', 'k',
+                                                   'b', 'r', 'o', 'w', 'n',
+                                                   'f', 'o', 'x'))
   }
 }
