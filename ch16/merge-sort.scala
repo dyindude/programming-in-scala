c31e6574263698d6814c031cbe06f016539974af
dyindude <dyindude@github.dyindude.net>
2018-05-09 11:07:01 -0500

    intSort/reverseIntSort examples ch16 p323-324

--- ch16/merge-sort.scala
+++ ch16/merge-sort.scala
@@ -17,5 +17,11 @@ object MergeSort {
         merge(msort(less)(ys), msort(less)(zs))
       }
     }
+
+    val intSort = msort((x: Int, y: Int) => x < y) _
+    val reverseIntSort = msort((x: Int, y: Int) => x > y) _
+    val mixedInts = List(4,1,9,0,5,8,3,6,2,7)
+    assert(intSort(mixedInts) == List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+    assert(reverseIntSort(mixedInts) == List(9, 8, 7, 6, 5, 4, 3, 2, 1, 0))
   }
 }
