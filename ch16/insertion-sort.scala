cbbd8e07878df19aa89d8a84fec5eef3299132bb
dyindude <dyindude@github.dyindude.net>
2018-05-09 10:08:27 -0500

    rev() function example using concatenation (:::) ch16 p316

--- ch16/insertion-sort.scala
+++ ch16/insertion-sort.scala
@@ -14,6 +14,10 @@ object InsertionSort {
         case List() => ys
         case x :: xs1 => x :: append(xs1, ys)
       }
+    def rev[T](xs: List[T]): List[T] = xs match {
+      case List() => xs
+      case x :: xs1 => rev(xs1) ::: List(x)
+    }
     
     val fruit: List[String] = List("apples", "oranges", "pears")
     val a :: b :: rest = fruit
