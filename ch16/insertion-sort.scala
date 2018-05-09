59e423a5324b400966dccc45a5dd74d669e1e162
dyindude <dyindude@github.dyindude.net>
2018-05-09 09:58:24 -0500

    append method example ch16 p314

--- ch16/insertion-sort.scala
+++ ch16/insertion-sort.scala
@@ -9,6 +9,11 @@ object InsertionSort {
       case y :: ys => if (x <= y) x :: xs
                       else y :: insert(x, ys)
     }
+    def append[T](xs: List[T], ys: List[T]): List[T] =
+      xs match {
+        case List() => ys
+        case x :: xs1 => x :: append(xs1, ys)
+      }
     
     val fruit: List[String] = List("apples", "oranges", "pears")
     val a :: b :: rest = fruit
