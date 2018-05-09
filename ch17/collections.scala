0260d912442e3128a8345d8fd66b21d4ccac2ad7
dyindude <dyindude@github.dyindude.net>
2018-05-09 16:22:37 -0500

    map operations table ch17 p350-351, some ops failing assert tests, will pick up tomorrow

--- ch17/collections.scala
+++ ch17/collections.scala
@@ -13,15 +13,15 @@ object Collections {
 
     //sets common operations table 17.1 ch17 p 348
     //immutable
-    val nums = Set(1, 2, 3)
-    assert(nums.toString == "Set(1, 2, 3)")
-    assert((nums + 5) == Set(1, 2, 3, 5))
-    assert((nums - 3) == Set(1, 2))
-    assert((nums ++ List(5,6)) == Set(1, 2, 3, 5, 6))
-    assert((nums -- List(1,2)) == Set(3))
-    assert((nums & Set(1, 3, 5, 7)) == Set(1,3)) // intersection
-    assert(nums.size == 3)
-    assert(nums.contains(3) == true)
+    val setNums = Set(1, 2, 3)
+    assert(setNums.toString == "Set(1, 2, 3)")
+    assert((setNums + 5) == Set(1, 2, 3, 5))
+    assert((setNums - 3) == Set(1, 2))
+    assert((setNums ++ List(5,6)) == Set(1, 2, 3, 5, 6))
+    assert((setNums -- List(1,2)) == Set(3))
+    assert((setNums & Set(1, 3, 5, 7)) == Set(1,3)) // intersection
+    assert(setNums.size == 3)
+    assert(setNums.contains(3) == true)
     //mutable
     //import scala.collection,mutable (already up there for another example)
     //val words = mutable.Set.empty[String] //reusing this val because lazy
@@ -49,5 +49,32 @@ object Collections {
       counts
     }
     assert(countWords("See spot run! Run, Spot. Run!") == Map("spot" -> 2, "see" -> 1, "run" -> 3))
+
+    //maps common operations table ch17 p350-351
+    //immutable
+    val mapNums = Map("i" -> 1, "ii" -> 2)
+    assert((mapNums + ("vi" -> 6)) == Map("i" -> 1, "ii" -> 2, "vi" -> 6))
+    assert((mapNums - "ii") == Map("i" -> 1))
+    assert((mapNums ++ List("iii" -> 3, "v" -> 5)) == Map("i" -> 1, "ii" -> 2, "iii" -> 3, "v" -> 5))
+    assert((mapNums -- List("i", "ii")) == Map())
+    assert(mapNums.size == 2)
+    assert(mapNums.contains("ii") == true)
+    assert(mapNums("ii") == 2)
+    assert(mapNums.keys == Set("i", "ii")) // actually returns Iterable
+    assert(mapNums.keySet == Set("i", "ii")) // returns Set
+    //assert(mapNums.values == Set(1, 2)) // need to find proper way to test this
+    assert(mapNums.isEmpty == false)
+    //mutable
+    //import scala.collection,mutable (already up there for another example)
+    val mapMutableWords = mutable.Map.empty[String, Int]
+    mapMutableWords += ("one" -> 1)
+    assert(mapMutableWords.toString == "Map(one -> 1)")
+    mapMutableWords -= "one"
+    assert(mapMutableWords.toString == "Map()")
+    mapMutableWords ++= List("one" -> 1, "two" -> 2, "three" -> 3)
+    //assert(mapMutableWords.toString == "Map(one -> 1, two -> 2, three -> 3)") //this failing too
+    mapMutableWords --= List("one", "two")
+    assert(mapMutableWords.toString == "Map(three -> 3)")
+
   }
 }
