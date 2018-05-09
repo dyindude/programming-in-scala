02a5f9329a39705ec13500fa5f63176af4041f4d
dyindude <dyindude@github.dyindude.net>
2018-05-09 15:18:23 -0500

    added examples from Sets operations table ch17 p348

--- ch17/collections.scala
+++ ch17/collections.scala
@@ -8,5 +8,31 @@ object Collections {
     for (word <- wordsArray)
       words += word.toLowerCase
     assert(words == Set("see", "run", "spot"))
+    words.clear
+
+    //sets common operations table 17.1 ch17 p 348
+    //immutable
+    val nums = Set(1, 2, 3)
+    assert(nums.toString == "Set(1, 2, 3)")
+    assert((nums + 5) == Set(1, 2, 3, 5))
+    assert((nums - 3) == Set(1, 2))
+    assert((nums ++ List(5,6)) == Set(1, 2, 3, 5, 6))
+    assert((nums -- List(1,2)) == Set(3))
+    assert((nums & Set(1, 3, 5, 7)) == Set(1,3)) // intersection
+    assert(nums.size == 3)
+    assert(nums.contains(3) == true)
+    //mutable
+    //import scala.collection,mutable (already up there for another example)
+    //val words = mutable.Set.empty[String] //reusing this val because lazy
+    words += "the"
+    assert(words == Set("the"))
+    words -= "the"
+    assert(words.toString == "Set()")
+    words ++= List("do", "re", "mi")
+    assert(words.toString == "Set(re, do, mi)")
+    words --= List("do", "re")
+    assert(words.toString == "Set(mi)")
+    words.clear
+    assert(words.toString == "Set()")
   }
 }
